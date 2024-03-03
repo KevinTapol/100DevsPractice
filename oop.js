cb9bc6a2b6d7b403b1f8bdfd264072a5f7acf5af
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2024-03-03 17:07:28 -0600

    creating a study material from 100Devs content

---
+++ oop.js
@@ -0,0 +1,12 @@
+/*
+The goal here is to demonstrate Object Oriented Programming Pillars of Encapsulation, Abstraction, Inheritance and Polymorphism.
+https://www.youtube.com/watch?v=yEhs4XtuAgA&ab_channel=LeonNoel
+Encapsulation - The process of storing functions (methods) with their associated data (properties) in one thing (object).
+    But why? - Made it easier to add new stuff, to read through what was already coded, and made it so you were not afraid to make changes.
+Abstraction - Hide details and show essentials.
+    But why? - Smaller more manageable pieces of code
+Inheritance - Make a class from another class for a hierarchy of classes that share a set of properties and methods.
+    But why? - Helps you eliminate redundant code.
+Polymorphism - Code written to use an interface automatically knows how to work with any number of different objects that provide the interface.
+    But why? - Helps you avoid if/else and switch cases. Also makes your code reusable.
+*/
